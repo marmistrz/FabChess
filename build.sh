5690c84b9237c30e1bba4d072fddf4b74aa95855
Marcin Mielniczuk <marmistrz.dev@zoho.eu>
2020-04-10 20:38:36 +0200

    Fix RUSTFLAGS for sp-wasm

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 set -e
 set -x
 . /usr/lib/emsdk/emsdk_env.sh
+export RUSTFLAGS="-C link-arg=-s -C link-arg=ALLOW_MEMORY_GROWTH=1"
 rustup run 1.38.0 cargo build --target=wasm32-unknown-emscripten "$@"
